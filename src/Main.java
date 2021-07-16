25b3479dacdf82043a1a5ed48888348391b63fbd
ChiEnergy <compresia91@gmail.com>
2021-07-16 18:59:29 +0300

    ДЗ №6 Выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,13 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[3];
-        employees[0] = new Employee("IvanovIvan","Engineer",
-                                    "ivan@mail.ru", "89879876543",
-                                    30000, 30);
-        employees[1] = new Employee("SergeySergey", "Proect",
-                                    "serg@mail.ru", "6547789765",
-                                    10000, 35);
-        employees[2] = new Employee("MarkMarkovich", "Program",
-                                    "mark@mail.ru", "8765454323",
-                                    40000, 45);
 
-        int minAge = 40;
-        for (Employee employee : employees) {
-            if (employee.age > minAge) {
-                employee.printInfo();
-                System.out.println();
-            }
-        }
+        Cat cat = new Cat(200);
+        Dog dog = new Dog(500, 10);
 
+        cat.run(150);
+        cat.swim(5);
+
+        dog.run(500);
+        dog.swim(9);
     }
 }
-
-
